38ded70a0e1b71477252f7b6adf052d773bcc7a0
cravista <gcravista@gmail.com>
2014-10-31 11:23:22 -0200

    Adicionados mais sintetizadores

--- init.js
+++ init.js
@@ -1,9 +1,9 @@
-var contextClass = (window.AudioContext or window.webkitAudioContext or window.mozAudioContext or window.oAudioContext or window.msAudioContext)
+var contextClass = window.AudioContext || window.webkitAudioContext || window.mozAudioContext || window.oAudioContext || window.msAudioContext;
 if(contextClass){
 // Web Audio API is available.
    window.context = new contextClass();
 }else{
 // Web Audio API is not available. Ask the user to use a
 // supported browser.
-   alert("BZIU!")
+   alert("BZIU!");
 }
